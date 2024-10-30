10d1ad6461ce05e3e464088ac5ac299082f81861
Zokky2e <icro4.gaming@hotmail.com>
2024-10-30 09:38:43 +0100

    adding basic styling files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
 import { AppRoutingModule } from "./app-routing.module";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
-	providers: [],
+	providers: [provideAnimationsAsync()],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
